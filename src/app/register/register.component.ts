98134be6236653a3d472dc2a1a31e72fcb5e6c1b
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 13:24:45 +0200

    RegisterComponent: ajout des méthodes initErrors, initControls et initUser

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
+	controls:any[];
+	errors:any[];
+	user:any;
 
-  constructor() { }
+  	constructor(private userService:UserService) { 
+  		this.initUser();
+  	}
 
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  	}
 
+  	validateEmail() {
+  		console.log(this.userService.users);
+  	}
+
+  	initUser() {
+  		this.user = {
+  			email: "",
+  			username: "",
+  			password: "",
+  			passwordConfirm: ""
+  		};
+  	}
+  	initControls() {
+  		this.controls['emailGroup'] = "form-group";
+  		this.controls['emailInput'] = "form-control";
+  		this.controls['usernameGroup'] = "form-group";
+  		this.controls['usernameInput'] = "form-control";
+  		this.controls['passwordGroup'] = "form-group";
+  		this.controls['passwordInput'] = "form-control";
+  		this.controls['passwordConfirmGroup'] = "form-group";
+  		this.controls['passwordConfirmInput'] = "form-control";
+  	}
+  	initErrors() {
+  		this.errors['email'] = [];
+  		this.errors['username'] = [];
+  		this.errors['password'] = [];
+  		this.errors['passwordConfirm'] = [];
+  	}
 }
