7531006243cf930056e57b5375ac55b70971b14b
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 16:04:38 +0200

    Routes: ajout de la route vers '/login'

--- src/app/config/routes.ts
+++ src/app/config/routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
 
 export const Routes: Route[] = [
 	{ path: 'register', component: RegisterComponent },
+	{ path: 'login', component: LoginComponent },
 ]
