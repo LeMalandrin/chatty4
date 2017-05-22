0407531e29d0f36ed0831f5f3fb0aecd8314ddb2
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 13:05:57 +0200

    UserService: alimentation d'une variable users (tableau de tous les utilisateurs de la base) dans le constructeur

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class UserService {
+	users:any[];
 
-  constructor() { }
+  	constructor(database:AngularFireDatabase) { 
+  		database.list('/users').subscribe(users=> {
+  			this.users = users
+  		});
+  	}
 
 }
