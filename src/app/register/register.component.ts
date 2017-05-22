58d7567e9dca5edb6fccb1df509fe721234f2051
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 14:24:23 +0200

    RegisterComponent: ajout des méthodes de contrôle du formulaire

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,21 +8,20 @@ import { UserService } from '../services/user/user.service';
   providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
-	controls:any[];
-	errors:any[];
+	controls:any[] = [];
+	errors:any[] = [];
 	user:any;
 
   	constructor(private userService:UserService) { 
-  		this.initUser();
+  		this.initUser(); //Initialisation de la variable user
+  		this.initControls(); //Initialisation des contrôles du formulaire
+  		this.initErrors(); //Initialisation des erreurs
   	}
 
   	ngOnInit() {
   	}
 
-  	validateEmail() {
-  		console.log(this.userService.users);
-  	}
-
+  	/* Méthode d'initialisation de la variable user */
   	initUser() {
   		this.user = {
   			email: "",
@@ -31,6 +30,7 @@ export class RegisterComponent implements OnInit {
   			passwordConfirm: ""
   		};
   	}
+  	/* Méthode d'initialisation des contrôles du formulaire */
   	initControls() {
   		this.controls['emailGroup'] = "form-group";
   		this.controls['emailInput'] = "form-control";
@@ -41,10 +41,126 @@ export class RegisterComponent implements OnInit {
   		this.controls['passwordConfirmGroup'] = "form-group";
   		this.controls['passwordConfirmInput'] = "form-control";
   	}
+  	/* Méthode d'initialisation des erreurs */
   	initErrors() {
   		this.errors['email'] = [];
   		this.errors['username'] = [];
   		this.errors['password'] = [];
   		this.errors['passwordConfirm'] = [];
   	}
+
+
+
+
+
+
+  	/* Méthode de validation de l'adresse email (sur keyup) */
+  	validateEmail() {
+  		this.errors['email'] = [];
+  		if(this.user.email.length > 0) {
+  			if(!this.userService.isValidEmail(this.user.email)) {
+  				this.errors['email'].push("L'adresse e-mail saisie est invalide");
+  			}
+  			if(this.userService.isExistingEmail(this.user.email)) {
+  				this.errors['email'].push("Un compte existant est déjà associé à cette adresse e-mail");
+  			}
+  			this.updateEmailControl();
+  		} else {
+  			this.resetEmailControl();
+  		}
+  	}
+  	updateEmailControl() {
+  		if(this.errors['email'].length>0) {
+  			this.controls['emailGroup'] = "form-group has-danger";
+  			this.controls['emailInput'] = "form-control form-control-danger";
+  		} else {
+  			this.controls['emailGroup'] = "form-group has-success";
+  			this.controls['emailInput'] = "form-control form-control-success";  			
+  		}
+  	}
+  	resetEmailControl() {
+		this.controls['emailGroup'] = "form-group";
+		this.controls['emailInput'] = "form-control";    		
+  	}
+  	/* Méthode de validation du nom d'utilisateur (sur keyup) */
+  	validateUsername() {
+  		this.errors['username'] = [];
+  		if(this.user.username.length > 0) {
+  			if(!this.userService.isValidUsername(this.user.username)) {
+  				this.errors['username'].push("Le nom d'utilisateur est invalide (5 à 15 caractères, majuscules, miniscules, chiffres et/ou tirets)");
+  			}
+  			if(this.userService.isExistingUsername(this.user.username)) {
+  				this.errors['username'].push("Ce nom d'utilisateur existe déjà");
+  			}
+  			this.updateUsernameControl();
+  		} else {
+  			this.resetUsernameControl();
+  		}
+  	}
+  	updateUsernameControl() {
+  		if(this.errors['username'].length>0) {
+  			this.controls['usernameGroup'] = "form-group has-danger";
+  			this.controls['usernameInput'] = "form-control form-control-danger";
+  		} else {
+  			this.controls['usernameGroup'] = "form-group has-success";
+  			this.controls['usernameInput'] = "form-control form-control-success";  			
+  		}
+  	}
+  	resetUsernameControl() {
+		this.controls['usernameGroup'] = "form-group";
+		this.controls['usernameInput'] = "form-control";    		
+  	}
+  	/* Méthodes de validation du mot de passe (sur keyup) */
+  	validatePassword() {
+  		this.errors['password'] = [];
+  		if(this.user.password.length > 0) {
+  			if(!this.userService.isValidPassword(this.user.password)) {
+  				this.errors['password'].push("Le mot de passe est invalide (6 à 20 caractères, majuscules, minuscules et/ou chiffres)");
+  			}
+  			this.updatePasswordControl();
+  		} else {
+  			this.resetPasswordControl();
+  		}
+  		this.validatePasswordConfirm();
+  	}
+  	updatePasswordControl() {
+  		if(this.errors['password'].length>0) {
+  			this.controls['passwordGroup'] = "form-group has-danger";
+  			this.controls['passwordInput'] = "form-control form-control-danger";
+  		} else {
+  			this.controls['passwordGroup'] = "form-group has-success";
+  			this.controls['passwordInput'] = "form-control form-control-success";  			
+  		}
+  	}
+  	resetPasswordControl() {
+		this.controls['passwordGroup'] = "form-group";
+		this.controls['passwordInput'] = "form-control";    		
+  	}
+  	/* Méthodes de validation de la confirmation du mot de passe (sur keyup) */
+  	validatePasswordConfirm() {
+  		this.errors['passwordConfirm'] = [];
+  		if(this.user.passwordConfirm.length > 0) {
+  			if(!this.userService.isValidPasswordConfirm(this.user.password, this.user.passwordConfirm)) {
+  				this.errors['passwordConfirm'].push("Les 2 mots de passe ne correspondent pas");
+  			}
+  			this.updatePasswordConfirmControl();
+  		} else {
+  			this.resetPasswordConfirmControl();
+  		}
+  	}
+  	updatePasswordConfirmControl() {
+  		if(this.errors['passwordConfirm'].length>0) {
+  			this.controls['passwordConfirmGroup'] = "form-group has-danger";
+  			this.controls['passwordConfirmInput'] = "form-control form-control-danger";
+  		} else {
+  			this.controls['passwordConfirmGroup'] = "form-group has-success";
+  			this.controls['passwordConfirmInput'] = "form-control form-control-success";  			
+  		}
+  	}
+  	resetPasswordConfirmControl() {
+		this.controls['passwordConfirmGroup'] = "form-group";
+		this.controls['passwordConfirmInput'] = "form-control";    		
+  	}
+
+
 }
