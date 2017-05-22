a278b2de750c003f212fbff2dad3c2b0f5c9ed04
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 11:27:40 +0200

    Ajout de la navbar bootstrap dans le composant NavbarComposant

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+	isNavbarCollapsed:boolean = true;
+  	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  	}
 
 }
