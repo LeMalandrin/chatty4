c733fb4a80b87bea63242e95e8d497435a5f7c40
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 09:56:30 +0200

    Création du fichier de confichier de configuration de la base de données firebase

---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,8 @@
+export const databaseConfig { 
+    apiKey: "AIzaSyD7KilIhPIOunvD8KmFEeNquM7HjkEKJtk",
+    authDomain: "chatty4-82cdb.firebaseapp.com",
+    databaseURL: "https://chatty4-82cdb.firebaseio.com",
+    projectId: "chatty4-82cdb",
+    storageBucket: "chatty4-82cdb.appspot.com",
+    messagingSenderId: "453388544044"
+};
