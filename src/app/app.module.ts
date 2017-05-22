7ba892a622481de44e7e7df73d9cb4b95cbe5f2b
LeMalandrin <coucke.antony@gmail.com>
2017-05-22 11:26:49 +0200

    Ajout d'une instance du module bootstrap dans app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RouterModule } from '@angular/router';
 import { Routes } from './config/routes';
@@ -19,6 +20,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot(Routes),
 
     AngularFireModule.initializeApp(DatabaseConfig, 'chatty'),
